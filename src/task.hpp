1ef9e6c869ccd48c44d16c8ca0a01e93e4fc431f
Igor Botukhov <igor.botukhov@gmail.com>
2024-02-22 13:11:14 +0100

    Add 2 new methods

--- src/task.hpp
+++ src/task.hpp
@@ -1,7 +1,21 @@
 #include <string>
+#include <iostream>
 
 class Task {
     private:
         std::string name;
         bool status;
+    public:
+        Task(std::string name, bool status){
+            name = name;
+            status = status;
+        }
+
+        std::string get_name() const{
+            return name;
+        }
+
+        bool get_status() const{
+            return status;
+        }
 };
